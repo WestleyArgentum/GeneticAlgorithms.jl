4ab0cc1f1a6e4f16e826f1d4c91d9b8d145ddfd2
Westley <WestleyArgentum@gmail.com>
2013-10-13 18:48:35 -0700

    serial and parallel tests in a single file… easier to understand

--- test/testga.jl
+++ test/testga.jl
@@ -1,49 +1,65 @@
-module Testga
 
 using GA
 
+module testga
+
+    type TestMonster
+        genes
+    end
+
+    # -------
+
+    function create_entity(num)
+        TestMonster(num)
+    end
+
+    function eval_entity(entity)
+        println("score: ", entity.genes)
+        entity.genes
+    end
+
+    function review_entities(population)
+        best = population[end]
+
+        if best.entity.genes >= 16
+            return true
+        end
+
+        return false
+    end
+
+    function group_entities(population)
+        pop_count = length(population)
+        for i in 1:pop_count
+            produce([i, pop_count])
+        end
+    end
+
+    function crossover(entities)
+        TestMonster(max(entities[1].genes, entities[2].genes))
+    end
+
+    function mutate(entity)
+        if rand(Float64) > 0.5
+            entity.genes += 1
+        end
+    end
+
+end
+
 # -------
 
-type TestMonster
-    genes
+function test_serial()
+    GA.run(testga; initial_pop_size = 8)
 end
 
 # -------
 
-function create_entity(num)
-    TestMonster(num)
-end
+function test_parallel(; nprocs_to_add = 2)
+    addprocs(nprocs_to_add)
+    require("GA/test/testga.jl")
 
-function eval_entity(entity)
-    println("score: ", entity.genes)
-    entity.genes
-end
+    println("nprocs: $(nprocs())")
 
-function review_entities(population)
-    best = population[end]
-
-    if best.entity.genes >= 16
-        return true
-    end
-
-    return false
-end
-
-function group_entities(population)
-    pop_count = length(population)
-    for i in 1:pop_count
-        produce([i, pop_count])
-    end
-end
-
-function crossover(entities)
-    TestMonster(max(entities[1].genes, entities[2].genes))
-end
-
-function mutate(entity)
-    if rand(Float64) > 0.5
-        entity.genes += 1
-    end
-end
-
+    GA.run(testga; initial_pop_size = 8)
 end
