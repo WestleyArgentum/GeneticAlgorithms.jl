d557b01f9b6152794348c70e5c6bfadb1556aad4
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2017-11-16 23:27:25 +1100

    Add load path and use equalityga everywhere
    Not actually sure this is the best approach.

--- examples/example-parallel.jl
+++ examples/example-parallel.jl
@@ -1,8 +1,9 @@
 using GeneticAlgorithms
 addprocs(2)
-@everywhere include(joinpath(@__DIR__, "equalityga.jl"))
-
 println("nprocs: $(nprocs())")
+
+push!(LOAD_PATH, @__DIR__)
+@everywhere using equalityga
 
 model = runga(equalityga; initial_pop_size = 16)
 
